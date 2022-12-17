dbae20b714398c9578184303c8798eec0f39536d
Евгения Быкова <evgeniarry@mail.ru>
2022-12-18 02:36:24 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rangeSlider1 = new Syncfusion.Windows.Forms.Tools.RangeSlider();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rangeSlider1
+            // 
+            this.rangeSlider1.BeforeTouchSize = new System.Drawing.Size(250, 27);
+            this.rangeSlider1.ChannelHeight = 5;
+            this.rangeSlider1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
+            this.rangeSlider1.ForeColor = System.Drawing.Color.LightGray;
+            this.rangeSlider1.HighlightedThumbColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.rangeSlider1.Location = new System.Drawing.Point(761, 59);
+            this.rangeSlider1.Maximum = 24;
+            this.rangeSlider1.Minimum = 1;
+            this.rangeSlider1.Name = "rangeSlider1";
+            this.rangeSlider1.PushedThumbColor = System.Drawing.Color.MediumPurple;
+            this.rangeSlider1.RangeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.rangeSlider1.Size = new System.Drawing.Size(250, 27);
+            this.rangeSlider1.SliderMax = 24;
+            this.rangeSlider1.SliderMin = 1;
+            this.rangeSlider1.SliderSize = new System.Drawing.Size(13, 17);
+            this.rangeSlider1.TabIndex = 0;
+            this.rangeSlider1.Text = "rangeSlider1";
+            this.rangeSlider1.ThumbColor = System.Drawing.Color.Thistle;
+            this.rangeSlider1.ValueChanged += new System.EventHandler(this.rangeSlider1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(830, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 15);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 221);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1177, 432);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Показать все туры";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1170, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 49);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Фильтр";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(234, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(381, 74);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(1030, 72);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(542, 72);
+            this.monthCalendar1.MaxSelectionCount = 30;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(532, 659);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 49);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Показать все туры";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1275, 712);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rangeSlider1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Tools.RangeSlider rangeSlider1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Вызов метода для получения списка всех путевок, составленных менеджером
+        List<Tour> tours = Manager.Tours(Manager.Hotels());
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //запрет на добавление новых путевок
+            dataGridView1.AllowUserToAddRows = false;
+
+            //Заполение comboBox1 странами
+            Manager.Countries(comboBox1, Manager.country);         
+        }
+
+        private void rangeSlider1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(rangeSlider1.SliderMin + "-" + rangeSlider1.SliderMax);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Manager.DataView(tours, dataGridView1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<Tour> filteredTour = new List<Tour>();
+            if (comboBox1.SelectedItem == null)
+                MessageBox.Show("Выберите страну");
+            else if (monthCalendar1.SelectionStart < DateTime.Now.AddDays(1))
+                MessageBox.Show("Выбранная дата некорректна");
+            else
+            {
+                foreach (Tour p in tours)
+                {
+                    
+                    if ((p.TravelCountry.Equals(comboBox1.Text)) && (numericUpDown1.Value < p.NumberOfFree) &&
+                        (p.Start >= monthCalendar1.SelectionStart) && (p.Start <= monthCalendar1.SelectionEnd) &&
+                        (p.CountOfNights >= rangeSlider1.SliderMin) && (p.CountOfNights <= rangeSlider1.SliderMax))
+                    {  
+                        if (comboBox2.SelectedItem == null)
+                            filteredTour.Add(p);
+                        else if (p.TravelCity.Equals(comboBox2.Text))
+                            filteredTour.Add(p);
+                    }
+                    
+                }
+            }
+            if (filteredTour.Count == 0)
+                MessageBox.Show("По вашему запросу ничего не найдено.");
+            Manager.DataView(filteredTour, dataGridView1);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Manager.Cities(comboBox2, comboBox1.Text);   
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form2 newForm = new Form2();
+            newForm.Show();
+        }
+    }
+}
